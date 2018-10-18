cf48127648191789dc5fc2610ca7d20075512bf6
Summertime <Summertime@users.noreply.github.com>
2018-10-19 01:54:36 +1030

    switching to nimble

---
+++ splat_json.nimble
@@ -0,0 +1,19 @@
+# Package
+
+version = "0.1"
+author = "Summertime"
+description = "Deserialization for the shell"
+license = "Unlicense"
+
+srcDir = "src"
+binDir = "bin"
+bin = @["splat_json"]
+skipExt = @["nim"]
+
+# Dependencies
+
+requires "nim >= 0.18.0"
+requires "commandeer >= 0.12.0"
+
+after build:
+    cpFile "bin/splat_json", "bin/splat-json"
--- src/splat_json.nim
+++ src/splat_json.nim
@@ -1,19 +1,10 @@
 import std / json
 import std / ospaths
-import std / parseopt
+import commandeer
 
-proc err_badargs(): void =
-    echo "splat-json [-p|--prefix:PREFIX]"
-    quit 2
-
-var prefix = ""
-
-for kind, key, val in getopt():
-    if (kind==cmdLongOption and key=="prefix" and val!="") or
-       (kind==cmdShortOption and key=="p" and val!=""):
-        prefix = val
-        continue
-    err_badargs()
+commandline:
+  option prefix, string, "prefix", "p", ""
+  errormsg "splat-json [-p|--prefix:PREFIX]"
 
 for key, val in stdin.readAll.parseJson:
     echo prefix & key & "=" & val.getStr($val).quoteShell()
