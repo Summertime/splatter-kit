401dba79e36fc7c9fb04f7d57f69626612f72462
Summertime <Summertime@users.noreply.github.com>
2018-10-19 21:28:16 +1030

    cleaning up indentatiion and formatting

--- splat_json.nimble
+++ splat_json.nimble
@@ -1,5 +1,3 @@
-# Package
-
 version = "0.1"
 author = "Summertime"
 description = "Deserialization for the shell"
@@ -9,8 +7,6 @@ srcDir = "src"
 binDir = "bin"
 bin = @["splat_json"]
 skipExt = @["nim"]
-
-# Dependencies
 
 requires "nim >= 0.18.0"
 requires "commandeer >= 0.12.0"
--- src/splat_json.nim
+++ src/splat_json.nim
@@ -3,8 +3,8 @@ import std / ospaths
 import commandeer
 
 commandline:
-  option prefix, string, "prefix", "p", ""
-  errormsg "splat-json [-p|--prefix:PREFIX]"
+    option prefix, string, "prefix", "p", ""
+    errormsg "splat-json [-p|--prefix:PREFIX]"
 
 for key, val in stdin.readAll.parseJson:
     echo prefix & key & "=" & val.getStr($val).quoteShell()
