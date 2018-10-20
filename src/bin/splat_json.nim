5f8059c6b3b075c4fd7bbf1a17ecb02c35449606
Summertime <Summertime@users.noreply.github.com>
2018-10-20 20:38:11 +1030

    Restructuring code for future additions

--- src/bin/splat_json.nim
+++ src/bin/splat_json.nim
@@ -1,14 +1,37 @@
 import std / json
+import std / options
 import std / ospaths
+import std / strformat
+import std / strutils
+
 import commandeer
 
+
 commandline:
-    option prefix, string, "prefix", "p", ""
+    option prefix, string, "prefix", "p", "JSON_"
     exitoption "version" ,"V", "0.2"
     exitoption "help", "h", "usage: splat-json [-p|--prefix:PREFIX]"
 
-for key, val in stdin.readAll.parseJson:
-    let name = prefix & key
-    if name != name.quoteShell():
-        continue
-    echo name & "=" & val.getStr($val).quoteShell()
+
+proc cleanValue(n: JsonNode): Option[string] =
+    some n.getStr($n).quoteShell
+
+
+proc cleanKey(s: string): Option[string] =
+    if s.validIdentifier:
+        return some s
+
+
+var rootNode = stdin.readAll.parseJson
+
+
+case rootNode.kind:
+    of JObject:
+        for k, v in rootNode:
+            let key = cleanKey prefix & k
+            let val = cleanValue v
+            if key.isNone or val.isNone:
+                continue
+            echo &"{key.get}={val.get}"
+    else:
+        discard
